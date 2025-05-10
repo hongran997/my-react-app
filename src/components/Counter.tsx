30202d7e585f0d8d5a32fd6bbd8efe1619efcd4e
zhaohongran <zhaohongran1997@163.com>
2025-05-10 17:54:53 +0800

    Counter 组件优化

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,33 +1,31 @@
-import { useEffect, useState } from 'react'
+import { useLayoutEffect, useState } from 'react'
 
 export default function Counter({ seconds }: {seconds: number}) {
   const [second, setSecond] = useState(seconds);
   const [time, setTime] = useState({ hour: 0, min: 0, sec: 0 });
 
-  useEffect(() => {
-    if (second < 0) {
-      return;
+  useLayoutEffect(() => { 
+    if (second < 0) return;
+    // 为什么不是按照50ms 更新，而是1s 更新呢?
+    // 新定时器会从创建时开始计时，但会保持与原有的1秒间隔同步
+    // setInterval 的回调函数会在每次间隔到达时执行，而不是在创建时立即执行
+    const timer = setInterval(() => { 
+      // if (second < 0) { 
+      //   clearInterval(timer);
+      //   return;
+      // }
+      const hour = Math.floor(second / 3600);
+      const min = Math.floor((second - hour * 3600) / 60);
+      const sec = second - hour * 3600 - min * 60;
+      setSecond(prev => prev - 1);
+      setTime({hour, min, sec})
+    }, 1000)
+
+    return () => { 
+      clearInterval(timer);
     }
 
-    const timer = setInterval(() => {
-      if (second <= 0) {
-        clearInterval(timer);
-        return;
-      }
-
-      const hourUnit = 60 * 60, minUnit = 60;
-      const hour = Math.floor(second / hourUnit);
-      const min = Math.floor((second - hour * hourUnit) / minUnit);
-      const sec = second - hour * hourUnit - min * minUnit;
-      
-      setTime({ hour, min, sec });
-      setSecond(prev => prev - 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [second]);
+  }, [second])
 
   return (
     <div>
